9a4d953b79c775ed78796ffd7bf2600ea9896996
Tadea <tsimunoviczg@gmail.com>
2019-09-26 21:53:35 -0400

    set up controllers for beach and location

--- app/controllers/api/v1/beaches_controller.rb
+++ app/controllers/api/v1/beaches_controller.rb
@@ -1,2 +1,36 @@
 class Api::V1::BeachesController < ApplicationController
+    before_action :set_location
+
+    def index
+        @beaches = @location.beaches
+        render json: @beaches
+    end
+
+    def show
+        @beach = @location.beaches.new(beach_params)
+        render json: @beach
+    end
+
+    def create
+        @beach = Beach.new(beach_params)
+        if @beach.save
+           render json: @beach
+        else
+           render json: {error: 'Error creating beach'}
+        end
+    end
+
+    def destroy
+        
+    end
+
+    private
+    def set_location
+        @location = Location.find(params[:location_id])
+    end
+
+    def beach_params
+        params.require(:beach).permit(:location_id, :name, :summary, :region, :image_url)
+    end
+end
 end
--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::LocationsController < ApplicationController
+    before_action :set_location, only: [:show, :update, :destroy]
+
+    def index
+     @locatios = Location.all
+     render json: @locations
+    end
+
+    def create
+     @location = Location.new(location_params)
+     if @location.save
+        render json: @location
+     else
+        render json: {error: 'Error creating location'}
+     end
+    end
+
+    def show
+        render json: @location
+    end
+
+    def destroy
+        @location.destroy
+    end
+
+    private
+    def set_location
+        @location = Location.find_by(id: params[:id])
+    end
+
+    def location_params
+        params.require(:location).permit(:name, :city)
+    end
 end
